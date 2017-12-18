2c98dca1e3f56cd342fc9a3653fc432d68440b35
Rails for Vic <xumingyo@gmail.com>
2017-12-18 13:42:11 +0000

    env fixed

--- init.rb
+++ init.rb
@@ -6,5 +6,8 @@ folders.each do |folder|
   require_relative "#{folder}/init.rb"
 end
 
+ENV['AWS_ACCESS_KEY_ID'] = app.config.AWS_ACCESS_KEY_ID
+ENV['AWS_SECRET_ACCESS_KEY'] = app.config.AWS_SECRET_ACCESS_KEY
+ENV['AWS_REGION'] = app.config.AWS_REGION
 
 
