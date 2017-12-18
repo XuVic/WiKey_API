170dba9a29ab670bd4557cfd3cd4bf38b820cf2a
Rails for Vic <xumingyo@gmail.com>
2017-12-18 13:49:48 +0000

    fixed

--- application/controllers/app.rb
+++ application/controllers/app.rb
@@ -16,6 +16,10 @@ module WiKey
    
    route do |routing|
     app = Api
+    ENV['AWS_ACCESS_KEY_ID'] = app.config.AWS_ACCESS_KEY_ID
+    ENV['AWS_SECRET_ACCESS_KEY'] = app.config.AWS_SECRET_ACCESS_KEY
+    ENV['AWS_REGION'] = app.config.AWS_REGION
+
    
      routing.root do
        { 'message' => "WiKey API v0.1 up in #{app.environment}." }
--- config.ru
+++ config.ru
@@ -2,9 +2,5 @@
 
 require_relative './init.rb'
 
-ENV['AWS_ACCESS_KEY_ID'] = app.config.AWS_ACCESS_KEY_ID
-ENV['AWS_SECRET_ACCESS_KEY'] = app.config.AWS_SECRET_ACCESS_KEY
-ENV['AWS_REGION'] = app.config.AWS_REGION
-
 run WiKey::Api.freeze.app
 
